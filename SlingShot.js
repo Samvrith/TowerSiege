67f0493646a7931c588dd77f4b18d9a8c3d50a0c
Samvrith <71236082+Samvrith@users.noreply.github.com>
2020-11-05 21:33:27 -0600

    Add files via upload

--- SlingShot.js
+++ SlingShot.js
@@ -4,25 +4,25 @@ class Slingshot{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 1
         }
-        this.pointB=pointB;
         this.sling = Constraint.create(options);
+        this.pointB=pointB;
         World.add(world, this.sling);
+
     }
-    fly()
-    {
-        this.sling.bodyA= null;
+    fly(){
+        this.sling.bodyA =null;
     }
-    display()
-    {
-        if(this.sling.bodyA)
-        {
+    display(){
+        if(this.sling.bodyA){
         var pointA = this.sling.bodyA.position;
         var pointB = this.pointB;
+        
         strokeWeight(4);
+        stroke("turquoise");
         line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
-    }
+        }
     
 }
